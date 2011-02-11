d0ee817467130b758c52b695c96b1c24213a0570
brunosan <brunosan@gmail.com>
2011-02-10 22:11:27 -0500

    scaffolding q/a votes users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Utilus::Application.config.secret_token = 'a268f0b51151c9731b0c9d2d1157ae4567069660e8167b26556d348143e49f895236f24c875f7bc646f86be7876751a489699ee7bc8c00e34310bcf6616918f0'
