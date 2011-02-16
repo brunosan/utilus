6a926ee4e8d2a3d42c2ea629a4d823edc85f27de
brunosan <brunosan@gmail.com>
2011-02-16 11:29:50 -0500

    removed actions for votes and flags

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -10,32 +10,6 @@ class FlagsController < ApplicationController
     end
   end
 
-  # GET /flags/1
-  # GET /flags/1.xml
-  def show
-    @flag = Flag.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @flag }
-    end
-  end
-
-  # GET /flags/new
-  # GET /flags/new.xml
-  def new
-    @flag = Flag.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @flag }
-    end
-  end
-
-  # GET /flags/1/edit
-  def edit
-    @flag = Flag.find(params[:id])
-  end
 
   # POST /flags
   # POST /flags.xml
@@ -49,31 +23,4 @@ class FlagsController < ApplicationController
       end
   end
 
-  # PUT /flags/1
-  # PUT /flags/1.xml
-  def update
-    @flag = Flag.find(params[:id])
-
-    respond_to do |format|
-      if @flag.update_attributes(params[:flag])
-        format.html { redirect_to(@flag, :notice => 'Flag was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @flag.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /flags/1
-  # DELETE /flags/1.xml
-  def destroy
-    @flag = Flag.find(params[:id])
-    @flag.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(flags_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
